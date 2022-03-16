62caec71cc072740aad465ac5cf1731c55f081d9
MildMax <joseph.s.burns93@gmail.com>
2022-03-16 15:08:21 -0400

    made components on profile configurable

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './profile.css';
 import CommentList from "./CommentList";
 import LikedList from "./LikedList";
 import MusicList from "./MusicList";
 
 const ProfileScreen = () => {
+
+    const [content, setContent] = useState('comments');
+
+    const isCurrentUser = false;
+    const isMusician = false;
+    const isFollowing = false;
+
+    const renderContent = (content) => {
+        switch (content) {
+            case 'likes':
+                return (<LikedList/>);
+            case 'music':
+                return (<MusicList/>);
+            default:
+                return (<CommentList/>)
+        }
+    }
+
+    const renderNav = (currentContent, sectionContent, label) => {
+        if (currentContent === sectionContent) {
+            return(
+                <li className="nav-item wd-nav-item-width">
+                    <button onClick={() => setContent(sectionContent)}
+                            className="wd-fg-color-white wd-nav-link-button wd-active-link">
+                        {label}
+                    </button>
+                </li>
+            );
+        } else {
+            return(
+                <li className="nav-item wd-nav-item-width">
+                    <button onClick={() => setContent(sectionContent)}
+                            className="wd-fg-color-white wd-nav-link-button">
+                        {label}
+                    </button>
+                </li>
+            );
+        }
+    }
+
+    const renderPrivateInfo = () => {
+        if(isCurrentUser) {
+            return (
+                <>
+                    <i className="far fa-id-card ps-3  wd-font-size-16"/>
+                    <span className="ps-2  wd-font-size-16">Name</span>
+                    <i className="fa fa-birthday-cake ps-3  wd-font-size-16"/>
+                    <span className="ps-2  wd-font-size-16">DOB</span>
+                    <i className="fa fa-at ps-3  wd-font-size-16"/>
+                    <span className="ps-2  wd-font-size-16">Email</span>
+                </>
+            );
+        }
+    }
+
+    const renderMainInfoButton = () => {
+        if (isCurrentUser) {
+            return (<button className="btn btn-dark wd-username-button">Edit Profile</button>)
+        }
+        else if (isFollowing) {
+            return (
+                <button className="btn btn-dark wd-username-button">Unfollow</button>
+            );
+        } else {
+            return (
+                <button className="btn btn-dark wd-username-button">Follow</button>
+            );
+        }
+    }
+
     return(
         <div>
             <div className="wd-profile-header-info-dims wd-position-relative wd-display-flex pt-2">
@@ -15,7 +85,7 @@ const ProfileScreen = () => {
                     <div className="wd-position-relative">
                         <span className="wd-fg-color-white wd-font-size-26 wd-bold-font">Username</span>
                         <span className="wd-username-button-position ps-5">
-                            <button className="btn btn-dark wd-username-button">Follow</button>
+                            {renderMainInfoButton()}
                         </span>
                     </div>
                     <div>
@@ -31,12 +101,7 @@ const ProfileScreen = () => {
                 <div>
                     <i className="far fa-calendar wd-font-size-16"/>
                     <span className="ps-2  wd-font-size-16">Joined</span>
-                    <i className="far fa-id-card ps-3  wd-font-size-16"/>
-                    <span className="ps-2  wd-font-size-16">Name</span>
-                    <i className="fa fa-birthday-cake ps-3  wd-font-size-16"/>
-                    <span className="ps-2  wd-font-size-16">DOB</span>
-                    <i className="fa fa-at ps-3  wd-font-size-16"/>
-                    <span className="ps-2  wd-font-size-16">Email</span>
+                    {renderPrivateInfo()}
                 </div>
                 <div>
                     <div className="wd-bold-font wd-font-size-20">Bio</div>
@@ -45,28 +110,33 @@ const ProfileScreen = () => {
             </div>
             <div className="mt-3">
                 <ul className="nav justify-content-center">
-                    <li className="nav-item">
-                        <button className="wd-fg-color-white wd-nav-link-button wd-active-link">
-                            Comments
-                        </button>
-                    </li>
-                    <li className="nav-item ps-5">
-                        <button className="wd-fg-color-white wd-nav-link-button">
-                            Likes
-                        </button>
-                    </li>
-                    <li className="nav-item ps-5">
-                        <button className="wd-fg-color-white wd-nav-link-button">
-                            Music
-                        </button>
-                    </li>
+                    {renderNav(content, 'comments', 'Comments')}
+                    {renderNav(content, 'likes', 'Likes')}
+                    {isMusician && renderNav(content, 'music', 'Music')}
+                    {/*<li className="nav-item">*/}
+                    {/*    */}
+                    {/*    <button onClick={() => setContent('comments')}*/}
+                    {/*            className="wd-fg-color-white wd-nav-link-button wd-active-link">*/}
+                    {/*        Comments*/}
+                    {/*    </button>*/}
+                    {/*</li>*/}
+                    {/*<li className="nav-item ps-5">*/}
+                    {/*    <button onClick={() => setContent('likes')}*/}
+                    {/*            className="wd-fg-color-white wd-nav-link-button">*/}
+                    {/*        Likes*/}
+                    {/*    </button>*/}
+                    {/*</li>*/}
+                    {/*<li className="nav-item ps-5">*/}
+                    {/*    <button onClick={() => setContent('music')}*/}
+                    {/*            className="wd-fg-color-white wd-nav-link-button">*/}
+                    {/*        Music*/}
+                    {/*    </button>*/}
+                    {/*</li>*/}
 
                 </ul>
             </div>
             <div className="wd-content-section wd-fg-color-white ps-3 pe-3">
-                {/*<CommentList/>*/}
-                {/*<LikedList/>*/}
-                <MusicList/>
+                {renderContent(content)}
             </div>
         </div>
     );
