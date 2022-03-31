6cb82680a656b5e610facd4c2f4dee5f33aae585
MildMax <joseph.s.burns93@gmail.com>
2022-03-30 20:36:50 -0400

    added axios with credentials api object in backend-service

--- src/services/backend/backend-service.js
+++ src/services/backend/backend-service.js
@@ -1,27 +1,32 @@
 import axios from 'axios';
 
+const api = axios.create({
+    withCredentials: true,
+    origin: "localhost:3000"
+})
+
 const backendHost = "http://localhost:4000"
 
 const getUserProfile = async () => {
-    const userProfile = await axios.get(backendHost + '/profile');
+    const userProfile = await api.get(backendHost + '/profile');
     return userProfile.data;
 }
 
 const updateUserProfile = async (updatedProfileInfo) => {
-    await axios.put(backendHost + '/profile', updatedProfileInfo);
+    await api.put(backendHost + '/profile', updatedProfileInfo);
 }
 
 const getProfile = async (id) => {
     if (id === undefined) {
         return await getUserProfile();
     }
-    const profile = await axios.get(backendHost + '/profile/' + id);
+    const profile = await api.get(backendHost + '/profile/' + id);
     return profile.data;
 }
 
 const registerAdmin = async (key) => {
     let error = false;
-    await axios.post(backendHost + '/profile/register', {key: key}).catch(err => error = true)
+    await api.post(backendHost + '/profile/register', {key: key}).catch(err => error = true)
 
     return !error;
 }
