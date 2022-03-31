186cfc8ab7206f1012abad304ea27299f355c202
ShebnaMathew <shebna.mathew@gmail.com>
2022-03-31 14:55:47 -0400

    posts

--- src/components/NewsFeed/Post.js
+++ src/components/NewsFeed/Post.js
@@ -1,36 +1,64 @@
-import React from "react"; 
-import StackGrid from "react-stack-grid";
-import './newsfeed.css';
+import { useNavigate } from "react-router-dom";
+import PostList from "./PostList";
+
 
 const Post = (props) => {
+    const post = props.post;
+    console.log("post idx: ", post.idx)
+    console.log("posts: ", props.posts)
+    console.log("posts slice: ", props.posts.splice(post.idx+1,3))
+
+    const morePosts = props.posts.sort(() => .5 - Math.random()).slice(0, 3)
+
     return(
-        <StackGrid columnWidth={350}>
-            {props.posts.map((post) => {
-                return (
-                    <div class="card mb-2 me-3">
-                        <a href={post.artists[0].external_urls.spotify} target="_blank"> 
-                            <img src={post.images[1].url} class="card-img-top" alt="..."/>
-                            </a>
-                        <div class="card-body">
-                            <h6>{post.name} - {post.artists[0].name}</h6>
-                            <p>
-                                <span className="me-1"><i class="far fa-heart"></i></span>
-                                <span className="me-2">{post.reacts.like}</span>
-                                <span className="me-1"><i class="far fa-comment"></i></span>
-                                <span className="me-2">{post.reacts.comment}</span>
-                            </p>
+        <>
+        <div className="row mb-5 mt-5">
+            <div className="col-1">
+                <button className="btn btn-light wd-round-btn wd-shadow" onClick={() => {
+                        props.setPost('');
+                        props.setShowPost(false);
+                    }}><i class="fas fa-angle-left"></i></button>
+            </div>
+            <div class="col-10 card wd-shadow">
+                <div class="row">
+                    <div class="col-5">
+                    <a href={post.artists[0].external_urls.spotify} target="_blank"> 
+                        <img src={post.images[0].url} class="img-fluid rounded-start my-3" alt="..."/>
+                    </a>
+                    </div>
+                    <div class="col-7">
+                    <div class="card-body">
+                        <h4 class="card-title">{post.name}</h4>
+                        <h5 class="card-title">{post.artists[0].name}</h5>
+                        <p class="card-text mt-4">Released on  {post.release_date}</p>
+                        <p class="card-text">{post.total_tracks} tracks</p>
+                        <hr/>
+                        <h6>
+                            <span className="me-2"><i class="far fa-heart"></i></span>
+                            <span className="me-4">{post.reacts.like}</span>
+                            <span className="me-2"><i class="far fa-comment"></i></span>
+                            <span className="me-4">{post.reacts.comment}</span>
+                         </h6>
+                        <hr/>
+                        <div class="wd-post-max-height wd-post-overflow">
                             {post.comments.map((c) => {
-                                    return <p class="card-text">
+                                return <p class="card-text">
                                         <span className="wd-newsfeed-bold-text me-2">{c.name}</span>
                                         <span>{c.comment}</span>
                                     </p>
                             })}
                         </div>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
-                )
-            })}
-        </StackGrid>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h6 className="row mb-2 mt-2 justify-content-center">More posts</h6>
+        <PostList posts={morePosts} setShowPost={props.setShowPost} setPost={props.setPost}/>
+        </>
     )
+
 }
 
 export default Post;
--- src/components/NewsFeed/index.js
+++ src/components/NewsFeed/index.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import Post from "./Post";
+import PostList from "./PostList";
 import recentPosts from "./recents-DUMMY.json";
 
 const NewsFeed = () => {
+
+    const [showPost, setShowPost] = useState(false);
+    const [post, setPost] = useState('');
+
+    console.log("showPost: ", showPost);
+    console.log("post Id: ", post);
+    console.log("posstss: ", recentPosts);
+
     return(
-        <Post posts={recentPosts}/>
+        (!showPost) ? <PostList posts={recentPosts} setShowPost={setShowPost} setPost={setPost}/>: <Post post={post} posts={[...recentPosts]} setShowPost={setShowPost} setPost={setPost}/>
     )
 }
 
