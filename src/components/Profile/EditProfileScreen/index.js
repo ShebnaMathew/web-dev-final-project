092532539e1f618ed6180af497f16ec6da7219cb
MildMax <joseph.s.burns93@gmail.com>
2022-04-02 14:05:16 -0400

    refactored editProfile to use single setter for data

--- src/components/Profile/EditProfileScreen/index.js
+++ src/components/Profile/EditProfileScreen/index.js
@@ -13,16 +13,10 @@ const EditProfileScreen = () => {
 
     const [showRegisterArtist, setShowRegisterArtist] = useState(false);
     const [showRegisterAdmin, setShowRegisterAdmin] = useState(false);
-
-    const [name, setName] = useState("");
-    const [bio, setBio] = useState("");
-    const [website, setWebsite] = useState("");
-    const [dob, setDob] = useState("");
-    const [email, setEmail] = useState("");
     const [isArtist, setIsArtist] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
-    const [profilePicture, setProfilePicture] = useState("");
-    const [username, setUsername] = useState("");
+
+    const [profile, setProfile] = useState({})
 
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
@@ -31,23 +25,13 @@ const EditProfileScreen = () => {
     // dynamically retrieve user profile
     const getUserProfile = async () => {
         profileData = await getProfile(user._id);
-        setName(profileData.name);
-        setBio(profileData.bio);
-        setWebsite(profileData.website);
-        setDob(profileData.dob);
-        setEmail(profileData.email);
-        setIsArtist(profileData.isArtist);
-        setIsAdmin(profileData.isAdmin);
-        setProfilePicture(profileData.profilePicture);
-        setUsername(profileData.username);
+        setProfile(profileData);
     }
 
-    if (name === "") {
+    // force wait on render for edit profile
+    // prevents users from attempting to access and edit profile that does not belong to them
+    if (!profile._id) {
         getUserProfile();
-    }
-
-    const captureFieldChange = (event, setter) => {
-        setter(event.target.value)
     }
 
     const showRegisterArtistPopUp = () => {
@@ -100,7 +84,7 @@ const EditProfileScreen = () => {
             {renderRegisterAdminPopUp()}
             <div className="wd-profile-header-info-dims wd-position-relative wd-display-flex wd-main-outer-padding pt-2">
                 <div className=" wd-display-inline-block pe-2 wd-position-relative">
-                    <img className="img-fluid wd-profile-picture-dims wd-circle-image" src={profilePicture} alt=""/>
+                    <img className="img-fluid wd-profile-picture-dims wd-circle-image" src={profile.profilePicture ? profile.profilePicture : "/images/blank-profile-picture.png"} alt=""/>
                     <div className="wd-profile-picture-dims wd-edit-profile-picture-overlay-position">
                         <div className="wd-edit-profile-image-filter wd-circle-image wd-edit-border-transparent"/>
                         <button className="wd-edit-profile-overlay-button wd-edit-profile-picture-button">
@@ -110,14 +94,10 @@ const EditProfileScreen = () => {
                 </div>
                 <div className="wd-display-inline-block wd-position-relative wd-full-height wd-main-info-dims">
                     <div className="wd-display-conditional-block wd-edit-profile-button-position">
-                        <div className="wd-edit-profile-username-position wd-edit wd-fg-color-white wd-font-size-26 wd-bold-font">{username}</div>
+                        <div className="wd-edit-profile-username-position wd-edit wd-fg-color-white wd-font-size-26 wd-bold-font">{profile.username ? profile.username : ""}</div>
                         <Link to="/profile"
                               onClick={() => saveProfileDataAction(dispatch, {
-                                  name: name,
-                                  bio: bio,
-                                  website: website,
-                                  dob: dob,
-                                  email: email
+                                  ...profile
                               })}
                               className="btn btn-dark wd-edit-profile-header-button wd-edit-profile-button-display me-4">
                             Save Changes
@@ -132,36 +112,36 @@ const EditProfileScreen = () => {
                 <div className="wd-edit-profile-text-entry ps-2 pe-2 pt-1 pb-1">
                     <label htmlFor="name" className="wd-font-12">Name</label>
                     <textarea id="name"
-                              onChange={(event) => captureFieldChange(event, setName)}
+                              onChange={(event) => setProfile({...profile, name: event.target.value})}
                               rows={1}
-                              defaultValue={name ? name : ""}
+                              defaultValue={profile.name ? profile.name : ""}
                     />
                 </div>
                 <br/>
                 <div className="wd-edit-profile-text-entry ps-2 pe-2 pt-1 pb-1">
                     <label htmlFor="bio" className="wd-font-12">Bio</label>
                     <textarea id="bio"
-                              onChange={(event) => captureFieldChange(event, setBio)}
+                              onChange={(event) => setProfile({...profile, bio: event.target.value})}
                               rows={3}
-                              defaultValue={bio ? bio : ""}
+                              defaultValue={profile.bio ? profile.bio : ""}
                     />
                 </div>
                 <br/>
                 <div className="wd-edit-profile-text-entry ps-2 pe-2 pt-1 pb-1">
                     <label htmlFor="website" className="wd-font-12">Website</label>
                     <textarea id="website"
-                              onChange={(event) => captureFieldChange(event, setWebsite)}
+                              onChange={(event) => setProfile({...profile, website: event.target.value})}
                               rows={1}
-                              defaultValue={website ? website : ""}
+                              defaultValue={profile.website ? profile.website : ""}
                     />
                 </div>
                 <br/>
                 <div className="wd-edit-profile-text-entry ps-2 pe-2 pt-1 pb-1">
                     <label htmlFor="email" className="wd-font-12">Email</label>
                     <textarea id="email"
-                              onChange={(event) => captureFieldChange(event, setEmail)}
+                              onChange={(event) => setProfile({...profile, email: event.target.value})}
                               rows={1}
-                              defaultValue={email ? email : ""}
+                              defaultValue={profile.email ? profile.email : ""}
                     />
                 </div>
                 <br/>
@@ -170,8 +150,8 @@ const EditProfileScreen = () => {
                     <input id="dob"
                            type="date"
                            className="form-control ps-0 pe-0"
-                           value={dob ? dob : ""}
-                           onChange={(event) => captureFieldChange(event, setDob)}
+                           value={profile.dob ? profile.dob : ""}
+                           onChange={(event) => setProfile({...profile, dob: event.target.value})}
                     />
                 </div>
             </div>
