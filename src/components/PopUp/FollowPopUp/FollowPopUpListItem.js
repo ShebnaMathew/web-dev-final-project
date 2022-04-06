11e4f4c3af302f79da116ab768b6071781fce8d9
MildMax <joseph.s.burns93@gmail.com>
2022-04-06 18:52:58 -0400

    fixedcorner case where user clicks on their own profile in the follow lists popups

--- src/components/PopUp/FollowPopUp/FollowPopUpListItem.js
+++ src/components/PopUp/FollowPopUp/FollowPopUpListItem.js
@@ -5,7 +5,7 @@ const FollowPopupListItem = ({setShowFollow, name, id}) => {
     return (
         <div className="wd-follow-popup-entry ps-3">
             <img className="img-fluid wd-full-height wd-circle-image" src="/images/blank-profile-picture.png" alt=""/>
-            <Link onClick={() => setShowFollow(false)} to={`/profile/${id}`} className="wd-display-inline-block ps-3">
+            <Link onClick={() => setShowFollow(false)} to={id ? `/profile/${id}` : '/profile' } className="wd-display-inline-block ps-3">
                 @{name}
             </Link>
         </div>
--- src/components/PopUp/FollowPopUp/index.js
+++ src/components/PopUp/FollowPopUp/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import FollowPopupListItem from "./FollowPopUpListItem";
+import {useSelector} from "react-redux";
 
 const FollowPopUpList = ({
     setShowFollow,
     followers,
     following,
 }) => {
+
+    const user = useSelector((state) => state.user);
+
     return(
         <>
             { followers ?
                 followers.map(follow => {
-                    return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.follower_name} id={follow.follower_id}/>
+                    if (user._id === follow.follower_id) {
+                        return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.follower_name}/>
+                    } else {
+                        return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.follower_name} id={follow.follower_id}/>
+                    }
                 })
                 :
                 following.map(follow => {
-                    return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.followee_name} id={follow.followee_id}/>
+                    if (user._id === follow.followee_id) {
+                        return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.followee_name}/>
+                    } else {
+                        return <FollowPopupListItem setShowFollow={setShowFollow} name={follow.followee_name} id={follow.followee_id}/>
+                    }
                 })
             }
         </>
